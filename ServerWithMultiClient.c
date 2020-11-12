04c50abfee808ecb100dc702b12c7e5489f47e29
Carlos Hernandez <50882485+carlosdvdh@users.noreply.github.com>
2020-11-12 17:32:08 -0500

    Add files via upload

--- ServerWithMultiClient.c
+++ ServerWithMultiClient.c
@@ -1,5 +1,6 @@
 #include <string.h>
 #include<stdio.h>
+#include <pthread.h>
 #include<stdlib.h>
 #include<string.h>
 #include<sys/types.h>
@@ -10,14 +11,14 @@
 #include<stdbool.h>
 #define PORTNUM  5220 /* the port number the server will listen to*/
 #define DEFAULT_PROTOCOL 0  /*constant for default protocol*/
-
+#define SEMKEY ((key_t) 400L)
 #define NUM_CARDS 18
-
+pthread_t th1,th2; //threads
+pthread_mutex_t mutex;
 struct Player{
    int player_num;
    int points;
-};
-
+};struct Player player[5];
 char symbols[NUM_CARDS] = {'=', '=', '&', '&', '!', '!', '?', '?', '*', '*', '%', '%', '@', '@', '}', '}', '#', '#'};
 
 struct Card{
@@ -55,6 +56,23 @@ void assign_cards() {
       card_set[i].showing = false;
       letter++;
    }
+}
+
+void pick_two(char first, char second){
+  int findex = first - 97;
+  int sindex = second - 97;
+  if(card_set[findex].hidden_symbol == card_set[sindex].hidden_symbol)
+  {
+    printf("Correct\n");
+    card_set[findex].hidden_symbol = 'X';
+    card_set[sindex].hidden_symbol = 'X';
+  }
+  else
+  {
+    printf("WRONG\n");
+    printf("%c ",card_set[findex].hidden_symbol);
+    printf("%c\n",card_set[sindex].hidden_symbol);
+  }
 }
 
 char buffer2[256];
@@ -104,12 +122,79 @@ void write_board (int sock) {
       exit(1);
    }
 }
-
+int order = 0;
+int neworder = 0;
+void * handle_connection(void* p_newsockfd)
+{
+       int newsockfd = *((int*)p_newsockfd);
+       char buffer[256];
+       bool playing = false;
+       int status;
+       int res_quit = strcmp(buffer2,"quit\n");
+       int count = 0;
+       while(res_quit!=0)
+       {
+          if(playing)
+          {
+            char message[255] = "Hi there user ";
+            char result[50];
+            if(pthread_self() == th1)
+            {
+              strcpy(result,"1");
+            }
+            else if(pthread_self() == th2)
+            {
+              strcpy(result,"2");
+            }
+            strcat(message,result);
+            strcat(message,"\n");
+            status = write(newsockfd,message,strlen(message));
+          }
+          read_from(newsockfd);
+          
+          int res_ready = strcmp(buffer2,"ready\n");
+          if(res_ready != 0 && !playing) 
+          {
+             status= write(newsockfd, "Send message \'ready\' to begin game.\n", 36);
+ 
+             if (status < 0) {
+                perror("ERROR writing to socket");
+                exit(1);
+             }
+             printf("Client must send message \'ready\' to begin game.\n");
+          }
+          else if(playing)
+          { 
+            char first[255];  //prints out the card chosen
+            strcpy(first,buffer2);
+            printf("%s\n",first);
+          }
+          else if(!playing)
+          {
+             printf("%s", buffer2); // print ready
+             // ideally we would have a function that takes u into gameplay, but for now it just prints the board 8~)
+             // which also means that itll ask u to send a ready message for each turn rn
+             // ALSO ideally all this logic won't be in main bc it looks super messy
+             write_board(newsockfd);
+             // once we accept choices from the user, we can use ASCII value comparions to
+             // be sure choices are between 'a' and 'r'
+             playing = true;
+          }
+          res_quit = strcmp(buffer2,"quit\n");
+          // bzero(buffer2,256);
+       }
+       return NULL;
+}
 int main( int argc, char *argv[] ) {
-   int sockfd, newsockfd, portno, clilen;
+   int sockfd, newsockfd, portno, clilen, newsockfd2;
    char buffer[256];
    struct sockaddr_in serv_addr, cli_addr;
    int status, pid;
+   
+   
+   pthread_attr_t attr;
+   pthread_attr_init(&attr);
+   pthread_mutex_init(&mutex,NULL);
 
    assign_cards(); // initializes game board IMPORTANT
    
@@ -143,59 +228,27 @@ int main( int argc, char *argv[] ) {
    
    listen(sockfd,5);
    clilen = sizeof(cli_addr);
-   
    while (1) {
-      newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, 	&clilen);
-		
-      if (newsockfd < 0) {
-         perror("ERROR on accept");
-         exit(1);
-      }
+     if(order == 0)
+     {
+        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, 	&clilen);
+        if(newsockfd < 0){
+          perror("ERROR on accept");
+          exit(1);
+        }
+        pthread_create(&th1, &attr, *handle_connection, &newsockfd);
+        order++;
+     }
+     if(order == 1)
+     {
+        newsockfd2 = accept(sockfd, (struct sockaddr *) &cli_addr, 	&clilen);
+        if(newsockfd2 < 0){
+          perror("ERROR on accept");
+          exit(1);
+        }
+        pthread_create(&th2, &attr, *handle_connection, &newsockfd2);
+        order++;
+     }
       
-      /* Create child process */
-      pid = fork();
-		
-      if (pid < 0) {
-         perror("ERROR on fork");
-         exit(1);
-      }
-      
-      if (pid == 0) {
-         /* This is the client process */
-         int res_quit = strcmp(buffer2,"quit\n");
-         while(res_quit!=0)
-         {
-            read_from(newsockfd);
-            int res_ready = strcmp(buffer2,"ready\n");
-            if(res_ready != 0)
-            {
-               status= write(newsockfd, "Send message \'ready\' to begin game.\n", 36);
-   
-               if (status < 0) {
-                  perror("ERROR writing to socket");
-                  exit(1);
-               }
-               printf("Client must send message \'ready\' to begin game.\n");
-            }
-            else
-            {
-               printf("%s", buffer2); // print ready
-               // ideally we would have a function that takes u into gameplay, but for now it just prints the board 8~)
-               // which also means that itll ask u to send a ready message for each turn rn
-               // ALSO ideally all this logic won't be in main bc it looks super messy
-               write_board(newsockfd);
-               // once we accept choices from the user, we can use ASCII value comparions to
-               // be sure choices are between 'a' and 'r'
-            }
-            res_quit = strcmp(buffer2,"quit\n");
-            // bzero(buffer2,256);
-         }
-         close(sockfd);
-         exit(0);
-      }
-      else {
-         close(newsockfd);
-      }
-		
    } /* end of while */
 }
--- clientwithsocket.c
+++ clientwithsocket.c
@@ -2,7 +2,6 @@
 it also shows how the client connects to a server socket.
 and sends a message to it. the server must already be running
 on a machine. The name of this machine must be entered in the function gethostbyname in the code below. The port number where the server is listening is specified in PORTNUM. This port number must also be specified in the server code.
-
  * main program */
 
 #include<stdio.h>
@@ -26,6 +25,7 @@ void main()
    int  socketid;      /*will hold the id of the socket created*/
    int  status;        /* error status holder*/
    char buffer[256];   /* the message buffer*/
+   char buffer2[256];
    struct sockaddr_in serv_addr;
 
    struct hostent *server;
@@ -43,7 +43,7 @@ void main()
    /* before connecting the socket we need to set up the right     	values in the different fields of the structure server_addr 
    you can check the definition of this structure on your own*/
    
-    server = gethostbyname("osnode04"); 
+    server = gethostbyname("osnode05"); 
 
    if (server == NULL)
    {
@@ -76,15 +76,15 @@ void main()
    /* now lets send a message to the server. the message will be
      whatever the user wants to write to the server.*/
   
-   int res = strcmp(buffer,"quit\n");
+   int res = strcmp(buffer,"ready\n");
    while(res != 0)
    {
-     printf("enter a nessage that you want the server to receive\n");
+     //printf("enter a nessage that you want the server to receive\n");
      bzero(buffer,256);
      fgets(buffer,255,stdin);
      status = write(socketid, buffer, strlen(buffer));
      
-     res = strcmp(buffer,"quit\n");
+     res = strcmp(buffer,"ready\n");
    
      if (status < 0)
      {
@@ -104,14 +104,51 @@ void main()
      }
      
      printf("%s\n",buffer);
-  }
-
-
-
+    }
+  
+    res = strcmp(buffer,"quit\n");
+    bzero(buffer,256);
+    status = read(socketid, buffer, 255);
+    printf("%s\n",buffer);
+     if (status < 0) {
+        perror("error while reading message from server");
+        exit(1);
+     }
+    while(res != 0)
+    {
+     printf("Enter first card: ");
+     bzero(buffer,256);
+     fgets(buffer,255,stdin);
+     status = write(socketid, buffer, strlen(buffer));
+     
+     if (status < 0)
+     {
+       printf("error while sending client message to server\n");
+     }
+     
+     printf("Enter second card: ");
+     bzero(buffer2,256);
+     fgets(buffer2,255,stdin);
+     status = write(socketid, buffer2, strlen(buffer2));
+     
+     if (status < 0)
+     {
+       printf("error while sending client message to server\n");
+     }
+     
+     bzero(buffer,256);
+     status = read(socketid, buffer, 255);
+     printf("%s\n",buffer);
+     if (status < 0) {
+        perror("error while reading message from server");
+        exit(1);
+     }
+      
+      res = strcmp(buffer,"quit\n");
+    }
 
    /* this closes the socket*/
    close(socketid);
 
   
 } 
-
